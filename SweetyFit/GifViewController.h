66ece65bfaa3b01257c2ee0e7f748447b63dedd7
DUO <zhaoduozd@outlook.com>
2018-03-07 11:31:37 +0800

    player

--- SweetyFit/GifViewController.h
+++ SweetyFit/GifViewController.h
@@ -9,5 +9,5 @@
 #import <UIKit/UIKit.h>
 
 @interface GifViewController : UIViewController
-
+@property(nonatomic, strong) UIImageView *imageview;
 @end
--- SweetyFit/GifViewController.m
+++ SweetyFit/GifViewController.m
@@ -10,18 +10,26 @@
 #import <AFNetworking/AFNetworking.h>
 
 @interface GifViewController ()
-
+@property(nonatomic, strong) NSMutableArray<UIImage *> *tmpArr;
 @end
 
 @implementation GifViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.imageview = [[UIImageView alloc] init];
+    self.imageview.frame = CGRectMake(10, 30, 300, 400);
+    
+    [self.view addSubview:_imageview];
+    [self.view setBackgroundColor:[UIColor whiteColor]];
+    //[self.imageview setBackgroundColor:[UIColor redColor]];
+    
+    __weak GifViewController *weakself = self;
     
     NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
     AFURLSessionManager *manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:configuration];
     
-    NSURL *URL = [NSURL URLWithString:@"http://127.0.0.1:3000/staticResource/test.gif"];
+    NSURL *URL = [NSURL URLWithString:@"http://s1.dwstatic.com/group1/M00/27/F2/816ff90306cecd113f0fd6e54acd2330.gif"];
     NSURLRequest *request = [NSURLRequest requestWithURL:URL];
     
     NSURLSessionDownloadTask *downloadTask = [manager downloadTaskWithRequest:request progress:nil destination:^NSURL *(NSURL *targetPath, NSURLResponse *response) {
@@ -30,12 +38,38 @@
         return [documentsDirectoryURL URLByAppendingPathComponent:[response suggestedFilename]];
     } completionHandler:^(NSURLResponse *response, NSURL *filePath, NSError *error) {
         NSLog(@"File downloaded to: %@", filePath);
+        
         NSData *data = [NSData dataWithContentsOfURL:filePath];
-        CGImageSourceRef sourec = CGImageSourceCreateWithData((__bridge CFDataRef)data, NULL);
+        CGImageSourceRef source = CGImageSourceCreateWithData((__bridge CFDataRef)data, NULL);
         
-        size_t count = CGImageSourceGetCount(sourec);
+        size_t count = CGImageSourceGetCount(source);
         
+        self.tmpArr = [[NSMutableArray alloc]init];
+        
+        for (size_t i = 0; i < count; i++)
+        {
+            CGImageRef imageRef = CGImageSourceCreateImageAtIndex(source, i, NULL);
+            
+            //3.将单帧图片转化为UIimage
+            UIImage * image = [UIImage imageWithCGImage:imageRef scale:[UIScreen mainScreen].scale orientation:UIImageOrientationUp];
+            
+            [self.tmpArr addObject:image];
+            
+            //释放imageRef
+            CGImageRelease(imageRef);
+        }
+        //释放sourec
+        CFRelease(source);
+        
+        NSLog(@"%@", self.tmpArr);
         NSLog(@"%lu", count);
+        
+        
+        weakself.imageview.animationImages = self.tmpArr;
+        weakself.imageview.animationDuration = 5;     //执行一次完整动画所需的时长
+        weakself.imageview.animationRepeatCount = 0;
+        
+        [weakself.imageview startAnimating];
 
     }];
     [downloadTask resume];
